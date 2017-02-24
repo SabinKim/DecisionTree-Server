f3662313b5c44e9a0d7ac5e526dd028c72e4d16e
Seth Battis <seth@battis.net>
2017-02-24 12:49:17 -0500

    More documentation (and correctness)

--- api.php
+++ api.php
@@ -1,21 +1,30 @@
 <?php
 
+/**
+ * based on https://www.leaseweb.com/labs/2015/10/creating-a-simple-rest-api-in-php/
+
+/* allow web apps not hosted on this server to access this API -- very, very dangerous! */
 header('Access-Control-Allow-Origin: *');
 
+/* open a connection the MySQL database server */
+// FIXME don't store the database credentials in plaintext in the repo!
 $sql = new mysqli('localhost', 'decisiontree', '2jaYLPn-esmu"18', 'decisiontree');
 
-// based on https://www.leaseweb.com/labs/2015/10/creating-a-simple-rest-api-in-php/
 
 /* parse the query out of the request string */
 $verb = strtoupper($_SERVER['REQUEST_METHOD']);
 $endpoint = explode('/', trim($_SERVER['PATH_INFO'], '/'));
 $parameters = json_decode(file_get_contents('php://input'), true);
+if (empty($parameters)) {
+    parse_str($_SERVER['QUERY_STRING'], $parameters);
+}
 
 /* set the table and key (if present) from first two tokens of end point */
 $table = $sql->escape_string(array_shift($endpoint));
 $key = $sql->escape_string(array_shift($endpoint));
 $subtable = $sql->escape_string(array_shift($endpoint));
 
+/* check if we have a subtable (only only valid query supports that at the moment) */
 if ($table == 'questions' && $subtable == 'answers') {
     $query = "SELECT a.* FROM `answers` AS a
                   LEFT JOIN `question-answer-pairing` AS qap ON qap.`answer` = a.`id`
@@ -43,7 +52,7 @@ switch ($verb) {
         $query = "INSERT INTO `$table` SET $set";
         break;
     case 'DELETE':
-        $query = "DELETE `$table` WHERE `id` = '$key'";
+        $query = "DELETE FROM `$table` WHERE `id` = '$key'";
         break;
 }
 
@@ -58,6 +67,8 @@ if (!$result) {
     die($sql->error);
 }
 
+/* send JSON response back to requester */
+header('Content-type: application/json');
 switch ($verb) {
     case 'GET':
         $response = [];
